15d0944cf748dbc476276269c40f1541b3748815
dejavutn <79543119+xdejavu@users.noreply.github.com>
2021-05-06 15:22:34 +0300

    Add files via upload

---
+++ Ders1/print_input.py
@@ -0,0 +1,14 @@
+print("hello python")
+print(""""Birden
+Fazla
+Satır
+Tek
+Print
+Kullanma""")
+adi=input("Adiniz:")
+soyadi=input("Soyadiniz:")
+numaraniz=input("Numaranız:")
+print("Girilen Bilgiler")
+print("Adınız",adi)
+print("Soyadınız",soyadi)
+print("Numaraniz",numaraniz)
