3c446cfd85e01d8f9cd2d77b1a3d9ede6ab9bb17
dejavutn <79543119+xdejavu@users.noreply.github.com>
2021-05-06 15:34:19 +0300

    Delete Ders_10 directory

--- Ders_10/Uygulama.py
+++
@@ -1,7 +0,0 @@
-#Toplama isimli fonksiyon tanımlayıp 3 parametre alsın. Buna gelen değerlerin toplamını ekrana yazsın.
-def toplama(a,b,c):
-    print("Sayılarınızın toplamı:",a+b+c)
-sayi1=int(input("1.sayıyı giriniz"))
-sayi2=int(input("2.sayıyı giriniz"))
-sayi3=int(input("3.sayıyı giriniz"))
-toplama(sayi1,sayi2,sayi3)
