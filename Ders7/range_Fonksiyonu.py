52b072ed28f286f8317dcd802933345a1c4bf0d3
dejavutn <79543119+xdejavu@users.noreply.github.com>
2021-05-06 15:27:19 +0300

    Add files via upload

---
+++ Ders7/range_Fonksiyonu.py
@@ -0,0 +1,4 @@
+print(*range(0,20))
+print(*range(0,21,2))
+liste=list(range(0,101)) #Liste yapmak için list() komutu kullanılır.
+print(liste)
---
+++ Ders7/uygulama.py
@@ -0,0 +1,5 @@
+#20'den geriye doğru ekrana yazdırma
+print(*range(20,0,-1)) #20'den 0'a kadar olan sayıları ekrana yazdırır.
+
+liste=list(range(20,0,-1))
+print(liste)
