03fa7c0d191a4cd1f36eff3fa15a29df270412ee
dejavutn <79543119+xdejavu@users.noreply.github.com>
2021-05-06 15:23:51 +0300

    Update Toplama.py

--- Ders2/Toplama.py
+++ Ders2/Toplama.py
@@ -1,4 +1,7 @@
-sayi1=input("1.sayıyı giriniz:")
-# inputan girilen değerler string türündedir. Eğer toplama çıkarma yapacaksanız int e çevirmelisiniz.
-sayi2=input("2.sayıyı giriniz:")
+sayi1=input("1. Sayıyı Giriniz:") #inputtan alınan veriler string tipindedir. eğer işlem yapacaksak int tipine dönüştürülmesi gerekir.
+sayi2=input("2. Sayıyı Giriniz:")
 print(int(sayi1)+int(sayi2))
+
+sayi1=int(input("1. Sayıyı Giriniz:"))
+sayi2=int(input("2. Sayıyı Giriniz:"))
+print(sayi1 + sayi2)
